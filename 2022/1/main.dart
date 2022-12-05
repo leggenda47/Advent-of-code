111d93dc401ee98aca1d7576bbc1c030f8f2fbb3
Petróczi Zoltán <lgnd@tutanota.com>
2022-12-05 14:23:28 +0100

    Separate first and second tasks

--- 2022/1/main.dart
+++ 2022/1/main.dart
@@ -6,9 +6,25 @@ extension on Iterable<int> {
   }
 }
 
-void main(List<String> args) {
-  final lines = File('./input.txt').readAsLinesSync();
+void first(List<String> lines) {
+  int maxCalories = 0;
+  int currentCalories = 0;
+  for (final line in lines) {
+    if (line.isEmpty) {
+      if (currentCalories > maxCalories) {
+        maxCalories = currentCalories;
+      }
+      currentCalories = 0;
+      continue;
+    }
 
+    final current = int.parse(line);
+    currentCalories += current;
+  }
+  print(maxCalories);
+}
+
+void second(List<String> lines) {
   final maxCalories = <int>[0];
   int currentCalories = 0;
   for (final line in lines) {
@@ -24,4 +40,11 @@ void main(List<String> args) {
   maxCalories.sort((a, b) => b.compareTo(a));
 
   print(maxCalories.take(3).sum());
+}
+
+void main(List<String> args) {
+  final lines = File('./input.txt').readAsLinesSync();
+
+  first(lines);
+  second(lines);
 }
